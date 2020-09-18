a97d86acb4c73ccfb49d68d47f612181c62f7bcf
Luka Zidar <luka.zidar@gauss.hr>
2020-09-18 15:18:03 +0200

    feat: styling

---
+++ components/pagination.vue
@@ -0,0 +1,209 @@
+<template>
+  <div class="pagination text-center bg-transparent  text-blue-700 font-semibold  py-2 px-4 border border-blue-500 rounded">
+    <div class="pages content-center ">
+      <button class="pagination_prev text-center bg-transparent  text-blue-700 font-semibold  py-2 px-4 border border-blue-500 rounded "
+        v-if="localPage !== 1"
+        :class="'pagination_end_page'"
+        :disabled="localPage === 1"
+        @click="setPage(1)"
+      >
+        1
+      </button>
+      <button
+        v-if="localPage !== 1"
+        class="pagination_prev text-center bg-transparent  text-blue-700 font-semibold  py-2 px-4 border border-blue-500 rounded"
+        :disabled="localPage === 1"
+        @click="setPage(localPage - 1)"
+      >
+        Nazad
+      </button>
+      <button
+        v-for="p in shownNumbers"
+        :key="`pagination-page-button-${p}`"
+        :class="p === localPage ? 'bg-grey-900 bg-transparent  text-blue-700 font-semibold  py-2 px-4 border border-blue-500 rounded  ' : ''  "
+        @click="setPage(p)"
+        v-text="p"
+      ></button>
+      <button
+        v-if="localPage !== lastPage"
+        class="pagination_prev text-center bg-transparent  text-blue-700 font-semibold  py-2 px-4 border border-blue-500 rounded"
+        :disabled="localPage === lastPage"
+        @click="setPage(localPage + 1)"
+      >
+        Dalje
+      </button>
+      <button class="pagination_prev text-center bg-transparent  text-blue-700 font-semibold  py-2 px-4 border border-blue-500 rounded"
+        v-if="localPage !== lastPage"
+        :disabled="localPage === lastPage"
+        @click="setPage(lastPage)"
+      >
+        75
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      default: 1
+    },
+    lastPage: {
+      type: Number,
+      default: 1
+    },
+    debounce: {
+      type: [String, Number],
+      default: 0
+    }
+  },
+  data() {
+    return {
+      localPage: 1,
+      switching: false,
+      timeout: null
+    }
+  },
+
+  computed: {
+    shownNumbers() {
+      const LENGTH = 9
+      const { lastPage } = this
+      const page = this.localPage
+
+      const firstPageInSight = page <= Math.floor(LENGTH / 2)
+      const lastPageInSight = lastPage - page <= Math.floor(LENGTH / 2)
+
+      const firstNumber = lastPageInSight ? lastPage - LENGTH + 1 : page - 4
+      const lastNumber = firstPageInSight ? LENGTH : page + 4
+
+      const first = Math.max(1, firstNumber)
+      const last = Math.min(lastPage, lastNumber)
+
+      const pages = []
+      for (let i = first; i <= last; i++) {
+        pages.push(i)
+      }
+      return pages
+    }
+  },
+  watch: {
+    value: {
+      handler: 'setPage',
+      immediate: true
+    }
+  },
+  methods: {
+    setPage(newPage) {
+      if (newPage === this.localPage) return
+      if (newPage > this.lastPage) return
+      if (newPage < 1) return
+
+      this.localPage = newPage
+      this.switchPage(newPage)
+    },
+    switchPage(newPage) {
+      if (this.switching) {
+        clearTimeout(this.timeout)
+        this.timeout = null
+      }
+
+      this.switching = true
+      this.timeout = setTimeout(() => {
+        this.$emit('input', newPage)
+        this.switching = false
+      }, Number(this.debounce))
+    }
+  }
+}
+</script>
+
+<style scoped>
+/*.pagination {*/
+/*  width: 100%;*/
+/*  padding: 15px 0;*/
+/*  margin-top: -1px;*/
+/*  font: 11px sans-serif;*/
+/*  line-height: 18px;*/
+/*}*/
+/*.pages {*/
+/*  width: 100%;*/
+/*  float: none;*/
+/*  text-align: center;*/
+/*  display: block;*/
+/*  padding-top: 25px;*/
+/*  padding-bottom: 25px;*/
+/*}*/
+/*.pagination button {*/
+/*  float: none;*/
+/*  outline: none;*/
+/*  -webkit-box-shadow: 0 2px 15px 0 rgba(0, 0, 0, 0.1);*/
+/*  box-shadow: 0 2px 15px 0 rgba(0, 0, 0, 0.1);*/
+/*  margin: 8px;*/
+/*  min-width: 40px;*/
+/*  height: 40px;*/
+/*  text-align: center;*/
+/*  border-radius: 100px;*/
+/*  line-height: 39px;*/
+/*  font-size: 16px;*/
+/*  font-weight: 100;*/
+/*  border: 1px solid rgb(238, 238, 238);*/
+/*  -webkit-box-sizing: border-box;*/
+/*  box-sizing: border-box;*/
+/*  display: inline-block;*/
+/*  font-family: Circular, Arial, Helvetica, sans-serif;*/
+/*  color: rgb(51, 51, 51);*/
+/*  padding: 0 10px;*/
+/*  &.active {*/
+/*    background: linear-gradient(*/
+/*        45deg,*/
+/*        rgb(253, 109, 88) 0,*/
+/*        rgb(248, 140, 35) 100%*/
+/*    );*/
+/*    -webkit-transition: background 0.4s ease-in;*/
+/*    -o-transition: background 0.4s ease-in;*/
+/*    transition: background 0.4s ease-in;*/
+/*    color: white;*/
+/*    border-color: rgba(0, 0, 0, 0);*/
+/*    border: 0;*/
+/*    font-weight: bold;*/
+/*  }*/
+/*}*/
+/*.pagination_prev {*/
+/*  padding: 0 25px !important;*/
+/*  width: auto;*/
+/*  border-radius: 100px !important;*/
+/*  font-size: 13px !important;*/
+/*  font-weight: 100 !important;*/
+/*}*/
+/*.pagination_end_page {*/
+/*  float: none;*/
+/*  outline: none;*/
+/*  -webkit-box-shadow: 0 2px 15px 0 rgba(0, 0, 0, 0.1);*/
+/*  box-shadow: 0 2px 15px 0 rgba(0, 0, 0, 0.1);*/
+/*  margin: 8px;*/
+/*  min-width: 40px;*/
+/*  height: 40px;*/
+/*  text-align: center;*/
+/*  border-radius: 100px;*/
+/*  line-height: 39px;*/
+/*  font-size: 16px;*/
+/*  font-weight: 100;*/
+/*  border: 1px solid rgb(238, 238, 238);*/
+/*  -webkit-box-sizing: border-box;*/
+/*  box-sizing: border-box;*/
+/*  display: inline-block;*/
+/*  font-family: Circular, Arial, Helvetica, sans-serif;*/
+/*  color: rgb(51, 51, 51);*/
+/*  padding: 0 10px;*/
+/*}*/
+/*.pagination img {*/
+/*  padding: 0;*/
+/*  border: 0;*/
+/*  float: none;*/
+/*  margin: 0;*/
+/*  display: inline-block;*/
+/*  border-radius: 5px;*/
+/*}*/
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,80 +2,90 @@
   <section class="text-gray-700 body-font">
     <h1 class="text-2xl font-semibold uppercase text-center py-10">Nekretnine</h1>
     <div class="container px-6 mx-auto">
-      <div class="flex flex-wrap -m-4" >
-        <div class="p-4 md:w-1/2 lg:w-1/3 sm:w-1/1" v-for="house in houses" :key="house.id" v-if="house.cover && house.cover.id" >
+      <div class="flex flex-wrap -m-4">
+        <div class="p-4 md:w-1/2 lg:w-1/3 sm:w-1/1" v-for="house in houses" :key="house.id"
+             v-if="house.cover && house.cover.id">
           <div class="h-full border-2 border-gray-200 rounded-lg overflow-hidden">
-            <img class="h-48 w-full object-cover object-center" :src="'http://homehapp-api.jsteam.gaussx.com/api/media/' + house.cover.id+'/small' " alt="cover">
+            <img class="h-48 w-full object-cover object-center"
+                 :src="'http://homehapp-api.jsteam.gaussx.com/api/media/' + house.cover.id+'/small' " alt="cover">
             <div class="p-6">
-              <h2 class="tracking-widest text-xs title-font font-medium text-gray-500 mb-1" v-if="house.protected===1">Status: Protected</h2>
-              <h2 class="tracking-widest text-xs title-font font-medium text-gray-500 mb-1" v-else-if="house.protected===0">Status: Active</h2>
-              <h1 class="title-font text-lg font-medium text-gray-900 mb-1 truncate uppercase">{{house.title}}</h1>
-              <p class="leading-relaxed mb-3">{{house.address}} {{house.address2}}</p>
+              <h2 class="tracking-widest text-xs title-font font-medium text-gray-500 mb-1" v-if="house.protected===1">
+                Status: Protected</h2>
+              <h2 class="tracking-widest text-xs title-font font-medium text-gray-500 mb-1"
+                  v-else-if="house.protected===0">Status: Active</h2>
+              <h1 class="title-font text-lg font-medium text-gray-900 mb-1 truncate uppercase">{{ house.title }}</h1>
+              <div class="flex justify-between">
 
-              <button class="bg-green-500 hover:bg-green-400 text-white font-bold py-1 px-5 border-b-2 border-green-700 hover:border-green-500 rounded-lg " v-if="house.locked===0" @click="singlePage(house)" >Open</button>
-              <button class="border-double border-2 border-gray-600 px-5 py-1 rounded-lg bg-gray-400  " v-else-if="house.locked===1">Open</button>
-
-
-<!--              <button class="px-4 py-1 rounded-lg " @click="singlePage(house)"
-                      :style="house.locked===0 ?'background: #538d22; color: white' : 'background: grey;  color: red'" :disabled="house.locked===1" >Open</button>-->
-
+                  <p class="leading-relaxed mb-3">{{ house.address }} {{ house.address2 }} </p>
+                  <div class="flex justify" >
+                  <img class="w-4 h-5 mr-2" src="@/assets/css/icon1.png" alt="icon">
+                    <span >{{house.bathrooms}}</span>
+                    <img class="w-4 h-5" src="@/assets/css/icon1.png" alt="icon">
+                </div>
+              </div>
+              <button
+                class="bg-green-500 hover:bg-green-400 text-white font-bold py-1 px-5 border-b-2 border-green-700 hover:border-green-500 rounded-lg "
+                v-if="house.locked===0" @click="singlePage(house)">Open
+              </button>
+              <button class="border-double border-2 border-gray-600 px-5 py-1 rounded-lg bg-gray-400  "
+                      v-else-if="house.locked===1">Open
+              </button>
             </div>
           </div>
         </div>
       </div>
+
     </div>
+    <pagination :debounce="300" @input="setPage" :value="localPagination.page" :last-page="localPagination.lastPage"/>
   </section>
 </template>
-<!--  <div class="container mx-auto py-16  ">
-    <h1 class="text-2xl font-semibold uppercase mb-6 text-center">Nekretnine</h1>
-    <div class="houses-container flex flex-wrap -mx-4 space-x-2 justify-center">
-      <div v-for="house in houses" :key="house.id" v-if="house.cover && house.cover.id"
-           class="w-full md:w-1/4 px-2 mb-12 sm:w-1/3 lg:w-1/5 no-underline h-full border-2 border-gray-400 rounded-lg overflow-hidden ">
-        <img class=" object-cover h-48 w-full"
-             :src="'http://homehapp-api.jsteam.gaussx.com/api/media/' + house.cover.id + 'small'" alt="cover">
-        <div class="text-black font-semibold text-lg truncate ...">
-          {{ house.title }}
-        </div>
-
-        <div class="text-gray-700 truncate ...  pb-1">{{ house.address }} {{ house.address2 }}</div>
-        <button class="px-4 py-1 rounded-lg " @click="singlePage(house)"
-                :style="house.locked===1 ? 'background: grey; color: red' : 'background: #538d22;  color: white'"
-                :disabled="house.locked===1">Open
-        </button>
-      </div>
-
-    </div>
-  </div>
-</template>-->
 
 <script>
 import axios from 'axios'
+import pagination from "@/components/pagination";
 
 export default {
-
+  components: {
+    pagination
+  },
   data() {
     return {
-      houses: []
-
+      houses: [],
+      localPagination: {
+        page: 1,
+      }
     }
+
   },
 
   async mounted() {
-    try {
-      let res = await axios.get('http://homehapp-api.jsteam.gaussx.com/api/home?page=1&limit=10&sortBy=price&sortOrder=desc')
-      this.houses = res.data.data.data
-      console.log(this.houses)
-    } catch (
-      error
-      ) {
-      console.log(error)
-    }
+    this.getHouses()
   },
+
   methods: {
     singlePage(house) {
       this.$router.push(`/about/${house.id}`)
     },
-
+    setPage(newPage) {
+      this.localPagination.page = newPage
+      this.getHouses()
+    },
+    async getHouses() {
+      try {
+        let res = await axios.get('http://homehapp-api.jsteam.gaussx.com/api/home?&limit=10&sortBy=price&sortOrder=desc', {
+          params: {
+            page: this.localPagination.page
+          }
+        })
+        this.houses = res.data.data.data
+        this.localPagination = res.data.data.pagination
+        console.log(this.houses)
+      } catch (
+        error
+        ) {
+        console.log(error)
+      }
+    }
   }
 }
 </script>
